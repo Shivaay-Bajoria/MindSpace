c6f6d6963981953371a979610d5c1a457c18dab1
Shivaay Bajoria <shivaaybajoria@gmail.com>
2024-10-04 22:21:06 +0530

    feat: Creating a MindSpace

---
+++ app/(modal)/adjust-meditation-duration.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AdjustMethodDuration = () => {
+  return (
+    <View>
+      <Text>AdjustMethodDuration</Text>
+    </View>
+  )
+}
+
+export default AdjustMethodDuration
---
+++ app/(tabs)/meditate/[id].tsx
@@ -0,0 +1,130 @@
+import { View, Text, ImageBackground, Pressable } from 'react-native'
+import React, { useEffect, useState } from 'react'
+
+import MEDITATION_IMAGES from "@/constants/meditation-images"
+import AppGradient from '@/components/AppGradient'
+import { router, useLocalSearchParams } from 'expo-router'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import CustomButton from '@/components/CustomButton'
+import { Audio } from 'expo-av';
+
+import { MEDITATION_DATA, AUDIO_FILES } from '@/constants/meditation-data'
+
+
+
+const Meditate  = () => {
+  const { id } = useLocalSearchParams();
+
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setMeditating] = useState(false);
+  const [audioSound, setSound] = useState<Audio.Sound>();
+  const [isPlayingAudio, setPlayingAudio] = useState(false);
+
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    if (secondsRemaining == 0) {
+      setMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1)
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    }
+  }, [secondsRemaining, isMeditating]);
+
+  useEffect(() => {
+    return () => {
+      audioSound?.unloadAsync();
+    };
+  }, [audioSound]);
+
+  const toggleMeditationSessionStatus = async () => {
+    if (secondsRemaining === 0) setSecondsRemaining(10);
+
+    setMeditating(!isMeditating);
+
+    await toggleSound();
+  };
+
+  const toggleSound = async () => {
+    const sound = audioSound ? audioSound : await intializeSound();
+    const status = await sound?.getStatusAsync();
+
+    if(status?.isLoaded && !isPlayingAudio) {
+      await sound.playAsync();
+      setPlayingAudio(true);
+    } else {
+      await sound.pauseAsync();
+      setPlayingAudio(false);
+    }
+  };
+
+  const intializeSound = async () => {
+    const audioFileName = MEDITATION_DATA[Number(id) - 1].audio;
+    const { sound } = await Audio.Sound.createAsync(
+      AUDIO_FILES[audioFileName]
+    );
+
+    setSound(sound);
+    return sound;
+
+  };
+
+  
+
+  const formattedTimeMinutes = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+  const formattedTimeSeconds = String(
+    secondsRemaining % 60
+  ).padStart(2, "0");
+
+  return (
+    <View className="flex-1">
+        <ImageBackground 
+            source={MEDITATION_IMAGES[Number(id) - 1]} 
+            resizeMode="cover" 
+            className="flex-1"
+        >
+            <AppGradient colors={["transparent", "rgba(0, 0, 0, 0.8)"]}>
+                <Pressable
+                    onPress={() => router.back()}
+                    className="absolute top-16 left-6 z-10"
+                >
+                    <AntDesign name="leftcircleo" size={50} color="white" />
+                </Pressable>
+
+                <View className="flex-1 justify-center">
+                  <View className="mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center">
+                    <Text className="text-4xl text-blue-800 font-rmono">
+                      {formattedTimeMinutes}:{formattedTimeSeconds}
+                    </Text>
+                  </View>
+                </View>
+
+                <View className="mb-5">
+                  <CustomButton 
+                    title="Adjust Duration" 
+                    onPress={toggleMeditationSessionStatus} 
+                  />
+                  <CustomButton 
+                    title="Start Meditation" 
+                    onPress={toggleMeditationSessionStatus} 
+                    containerStyles="mt-4"
+                  />
+                </View>
+            </AppGradient>
+        </ImageBackground>
+      <Text> Meditate </Text>
+    </View>
+  )
+}
+
+export default Meditate 
